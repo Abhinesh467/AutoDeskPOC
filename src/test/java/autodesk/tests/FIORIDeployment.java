eb315d837279b446c302750f5f7d7cbe4ed09481
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-16 03:41:35 +0530

    folder structure

---
+++ src/test/java/autodesk/tests/FIORIDeployment.java
@@ -0,0 +1,21 @@
+package autodesk.tests;
+
+public class FIORIDeployment extends LocalFIORI{
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+	
+		DevFIORI dev = new DevFIORI();
+		dev.devFIORIInititation();
+		dev.validateDevFioriData();
+
+		if(DEVPassPercentage.equals("100%")) {
+			QAFIORI QA = new QAFIORI();
+			QA.qaFIORIInititation();
+			QA.validateQAFioriData();
+		}else {
+			System.out.println("Dev Testing Failed");
+		}
+	}
+
+}
