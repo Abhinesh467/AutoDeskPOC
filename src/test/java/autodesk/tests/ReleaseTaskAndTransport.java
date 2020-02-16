54273a938c0f1a963102f31cefea3b0c383826b1
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-16 23:25:41 +0530

    Update ReleaseTaskAndTransport.java

--- src/test/java/autodesk/tests/ReleaseTaskAndTransport.java
+++ src/test/java/autodesk/tests/ReleaseTaskAndTransport.java
@@ -147,7 +147,9 @@ public class ReleaseTaskAndTransport extends AutodeskBaseUtility {
 		driver.findElement(By.xpath("(//b[text()='Edit'])[1]")).click();
 		Thread.sleep(4000);
 		@SuppressWarnings("unused")
-		Thread.sleep(4000);		
+		Thread.sleep(4000);	
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,250)");
 		WebElement element = driver.findElement(By.xpath("(//*[@title='Select table row'])[1]"));
 		WebElement element1 = driver.findElement(By.xpath("(//*[contains(@id,'_Release') and @title='Release Transport Request'])[1]"));		
 		Thread.sleep(500); 		
